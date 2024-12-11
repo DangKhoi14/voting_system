b78da112df29f9f12f2ba95ff33944de8a2047df
Dangkhoi14 <dangkhoi111003@gmail.com>
2024-12-12 00:08:14 +0700

    Add data context

---
+++ voting_system_core/Data/VotingDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using voting_system_core.Models;
+
+namespace voting_system_core.Data
+{
+    public class VotingDbContext : DbContext
+    {
+        public VotingDbContext(DbContextOptions<VotingDbContext> options) : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<Poll> Polls { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
